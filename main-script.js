2895477296b7a6013274270918296456d26ac7b4
Ahmed Magdy Seleem <Ahmed.Magdy.S.19@Gmail.com>
2020-05-10 03:56:52 +0200

    pre-finish

---
+++ main-script.js
@@ -0,0 +1,11 @@
+/* let navbar = document.querySelector(".nav")
+let sticky = navbar.offsetTop;
+
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}) */
